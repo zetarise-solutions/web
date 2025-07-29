a20e111c9d07dc8e0521588293cb6b785dbf4cbf
Harish N <harishprp370@gmail.com>
2025-07-29 11:21:54 +0530

    database

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,10 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "zetarise";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-else {
-    $conn->set_charset("utf8mb4");
 }
 
